27a27be73fa5d73d1b471ba49775762af338e77f
VibesTribe <133037895+VibesTribe@users.noreply.github.com>
2025-09-23 13:44:22 -0400

    Update publish.js

--- scripts/publish.js
+++ scripts/publish.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import fs from "node:fs/promises";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import { listDirectories, loadJson, ensureDir } from "../lib/utils.js";
+import { listDirectories, loadJson, ensureDir } from "./lib/utils.js";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const ROOT_DIR = path.resolve(__dirname, "..");
